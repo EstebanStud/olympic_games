a927c7bdad94cc2cbad9c766d1c5f7657c9bd215
Esteban Vidal <esteban.vidal@edu.devinci.fr>
2022-07-21 21:32:31 +0200

    ADD - Avancement n°2

--- bot.js
+++ bot.js
@@ -1,17 +1,22 @@
 require('dotenv').config()
 const Discord = require('discord.js');
-const token = 'OTk5NzQ1ODU0NzE2MDA2NDUx.G9IRJP.H76XKCW70X-k8xMuwZ2Ck82uTUvDNtd6GKKL18';
+
 // const commandLoader = require('./commandLoader');
-// const MySqlConnector = require('./src/MySqlConnector');
-const { bdd } = require('./Commands/ApiDB');
+const MySqlConnector = require('./src/MySqlConnector');
+// const { bdd } = require('./Commands/ApiDB');
 const bot = new Discord.Client({
     intents: ['GUILD_MESSAGES', 'GUILDS', 'GUILD_MEMBERS']
 });
 
+MySqlConnector.connect();
+
 bot.on('messageCreate', async(message) => {
+    let sportName = "Natation 100M";
+    let classement = await MySqlConnector.executeQuery(`SELECT * FROM sports WHERE Sport_Name="${sportName}"`);
+
     if (!message.author.bot) {
         if (message.content === 'ping') {
-            bdd.getClassement('Natation 100M', async(classement) => { console.log(classement) })
+            console.log(classement)
             const channel = message.channel;
             await channel.send('Pong!');
 
